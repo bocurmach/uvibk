703aaa91f79100e912866cb913dcb6a6e6259d8e
andreas <andreas.bayer.pyhsics@gmail.com>
2024-06-30 13:53:55 +0200

    helper and api key read from file not in git

--- helper.py
+++ helper.py
@@ -1,5 +1,6 @@
 API_URL_IBK = 'https://uv-data.i-med.ac.at/public/data/?site=Innsbruck'
 WEB_URL_IBK = 'https://www.uv-index.at/station/?site=Innsbruck&prod=uve'
 
-TELEGRAM_TOKEN = '1913192851:AAHybJz7QrFxBaN-VIASwjt5R2NzvLrHmcA'
+TELEGRAM_TOKEN = open('./api_key').readline().rstrip('\n')
+print(TELEGRAM_TOKEN)
 CHANNEL_NAME = '@uvibk'
