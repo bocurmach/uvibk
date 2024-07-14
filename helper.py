466b4530e86e31cf638558f069f9a063dadec019
andreas <andreas.bayer.physics@gmail.com>
2024-07-14 23:58:59 +0200

    added extra api module

--- helper.py
+++
@@ -1,5 +0,0 @@
-WEB_URL_IBK = 'https://www.uv-index.at/station/?site=Innsbruck&prod=uve'
-
-TELEGRAM_TOKEN = open('./api_key').readline().rstrip('\n')
-print(TELEGRAM_TOKEN)
-CHANNEL_NAME = '@uvibk'
---
+++ uibkapi.py
@@ -0,0 +1,41 @@
+import time
+import json
+from urllib3.exceptions import MaxRetryError
+import requests
+API_URL_IBK = 'https://uv-data.i-med.ac.at/public/data/?site=Innsbruck'
+from datetime import datetime, timedelta
+
+
+def convert_time_stamps(time_stamps: list[str]):
+    for i in range(len(time_stamps)):
+        time_stamps[i] = datetime.strptime(
+            time_stamps[i], '%Y-%m-%dT%H:%M:%S+0000') + timedelta(hours=2)
+
+    return time_stamps
+
+
+def get_data_from_api():
+    retry_count = 0
+    while True:
+        try:
+            time.sleep(retry_count**2)
+            retry_count += 1
+
+            r = requests.get(API_URL_IBK)
+        except MaxRetryError as err:
+            print('MaxRetryError', err)
+        except Exception as err:
+            print(err)
+        else:
+            break
+
+    content = json.loads(r.content.decode('utf-8'))
+
+    uve = content['Innsbruck']['uve']
+
+    measurements = uve['measurement']
+
+    time_stamps = convert_time_stamps(uve['ts'])
+
+    return measurements, time_stamps
+
